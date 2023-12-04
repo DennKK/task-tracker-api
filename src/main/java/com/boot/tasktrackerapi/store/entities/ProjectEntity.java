1d99c5dfcb6dd77db31f876888635ca234722d3f
Denis Karpov <karpovdenis.al@gmail.com>
2023-12-05 00:08:45 +0300

    fields and annotations have been added to the class

--- src/main/java/com/boot/tasktrackerapi/store/entities/ProjectEntity.java
+++ src/main/java/com/boot/tasktrackerapi/store/entities/ProjectEntity.java
@@ -1,4 +1,39 @@
 package com.boot.tasktrackerapi.store.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Table(name="project")
+@Entity
 public class ProjectEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @Column(unique=true)
+    private String name;
+    @Builder.Default
+    private LocalDate createdAt = LocalDate.now();
+    private String description;
+    @Builder.Default
+    @OneToMany
+    private List<TaskStateEntity> taskStates = new ArrayList<>();
 }
